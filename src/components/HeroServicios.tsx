2a05f618d0f2891b4ba571b6f4f055c7af9c48b3
Rox Ram <39720515+roxRam16@users.noreply.github.com>
2026-09-22 06:20:41 -0600

    Updated Header.tsx

--- src/components/HeroServicios.tsx
+++ src/components/HeroServicios.tsx
@@ -1,8 +1,7 @@
 export function HeroServicios() {
   return (
-    <section className="relative flex min-h-[1380px] flex-col overflow-hidden bg-[#101979] sm:min-h-[1120px] lg:min-h-[900px]">
+    <section className="relative flex min-h-[1380px] flex-col overflow-hidden sm:min-h-[1120px] lg:min-h-[900px]">
       <div className="absolute inset-0 bg-no-repeat" style={{ backgroundImage: "url('/seccion01/principal_xl.png')", backgroundSize: '100% auto', backgroundPosition: 'top center' }} />
-      <div className="absolute inset-0 bg-gradient-to-b from-[#101979]/60 via-[#101979]/10 to-transparent" />
 
       <div className="relative z-10 flex flex-1 items-end px-5 pb-0 sm:px-10 lg:px-16 xl:px-20">
       </div>
